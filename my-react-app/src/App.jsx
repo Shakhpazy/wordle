0dc9bf0ea3b3c8d3023ee8c814bf3d65350ea5d5
yusuf04-s <shakhpazy@spu.edu>
2025-05-02 14:34:49 -0700

    have the overall setup of the app complete --> next I can fine tune the design and work on functionality

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,23 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const keyboard = [
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    ['Enter','z', 'x', 'c', 'v', 'b', 'n', 'm', 'Backspace'],
+  ];
 
   return (
     <>
       <div className="wrapper">
       <div className='row'>
-        <div className='box'></div>
-        <div className='box'></div>
-        <div className='box'></div>
-        <div className='box'></div>
-        <div className='box'></div>
-      </div>
-      <div className='row'>
-        <div className='box'></div>
-        <div className='box'></div>
-        <div className='box'></div>
+        <div className='box green'></div>
+        <div className='box yellow'></div>
+        <div className='box grey'></div>
         <div className='box'></div>
         <div className='box'></div>
       </div>
@@ -51,6 +50,31 @@ function App() {
         <div className='box'></div>
         <div className='box'></div>
       </div>
+      <div className='row'>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+        <div className='box'></div>
+      </div>
+      </div>
+
+      <div className='keyboard'>
+        {keyboard.map((row, rowIndex) => (
+          <div className='row' key={rowIndex}>
+            {row.map((key, keyIndex) => (
+              <div className='key' key={keyIndex}>
+                {key === 'Enter' ? (
+                  <div className='key-enter'>Enter</div>
+                ) : key === 'Backspace' ? (
+                  <div className='key-backspace'>Delete</div>
+                ) : (
+                  <div className='key-letter'>{key}</div>
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     </>
   )
